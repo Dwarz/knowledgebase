3554cd2356107eea687de5415c5ee062de2e80b0
Marlon <bla@bla.nl>
2014-12-02 22:37:59 +0100

    function relatie toegevoegd, zodat in model enkel de relatienummers (categorie en artikel) hoeven worden ingevuld, ipv meerdere regels per relatie

--- app.php
+++ app.php
@@ -2,6 +2,14 @@
 
 function spanTag ( $tekst ) {
 	return "<span class=\"tag\">" . $tekst . "</span>";
-	}
+}
 
+function relatie ($catid , $artid ) { // functie aanmaken die het categorie id en artikel id meekrijgt uit model.php
+	$rel=array ()	;		// array aanmaken om het in op te slaan
+	$rel["artikel"]= $artid;	// artikel id opslaan
+	$rel["categorie"]= $catid;	// categorie id opslaan
+	return $rel;			// de gehele array returnen (wordt gebruikt in model)
+}
+	
+	
 ?>
