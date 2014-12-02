a49d586150e161f8110a2bc536776839a62ad5b0
Marlon <bla@bla.nl>
2014-12-02 22:38:19 +0100

    zoeken binnen catlist toegevoegd door strpos in de url te gebruiken

--- catlist.php
+++ catlist.php
@@ -4,6 +4,11 @@ require "app.php";
 
 require "model.php";
 
+if (isset($_GET["search"])){
+	$search = $_GET["search"]; //hier wordt bepaald of er een zoekwoord ( ?search=zoekwoord ) is meegegeven in de url, zo ja, dan wordt de if uitgevoerd. Zo nee, dan doet ie er niks mee
+}
+
+
 $cats = array(); // array cats aanmaken
 foreach($categorien as $categorie ) {  //er wordt door alle categorieen heengelopen en die worden naar categorie gekopieerd
 $categorie["artikelen"]= array() ;
@@ -11,8 +16,14 @@ $categorie["artikelen"]= array() ;
 		if( $relatie["categorie"] == $categorie["id"] ){  // als het nummer van een van de relaties gelijk is aan het gevraagde id wordt het volgende uitgevoerd
 			$artikel_id = $relatie["artikel"]; // van de gevonden relatie stopt ie het artikelnummer in $artikel_id
 			$artikel = $artikelen[$artikel_id]; // het artikel met het id wordt in zn geheel in $artikel gestopt
-			
-			$categorie["artikelen"][] = $artikel; //$artikel wordt toegevoegd aan de eerste legen plaats in $categorie["artikelen"]
+				if (isset ($search)) { // controleren of er een zoekterm is ingegeven
+					if ( strpos( $artikel["title"] , $search )!==false ){ // controleren of de zoekterm met de title van het artikel matcht met strpos(zoekplaats , zoekterm) de !==false betekent dat ook als het op plaats 0 (eerste teken wat hij tegenkomt) wordt gevonden, het toch wordt doorgegeven.
+							$categorie["artikelen"][] = $artikel; //$artikel wordt toegevoegd aan de eerste lege plaats in $categorie["artikelen"]
+					}
+				}	
+				else {
+						$categorie["artikelen"][] = $artikel; //$artikel wordt toegevoegd aan de eerste lege plaats in $categorie["artikelen"]
+				}
 		}
 	}
 	$cats[] = $categorie; //de inhoud van categorie wordt in cats geplaatst
